2c953b894152a41822758afcd8d56e8154bebfb5
Sarkovic <srahman212074@bscse.uiu.ac.bd>
2022-09-19 04:43:55 +0600

    Edited

--- src/main/java/com/mahbubalam/blooddonationsystem/server/provider/ConnectionProvider.java
+++ src/main/java/com/mahbubalam/blooddonationsystem/server/provider/ConnectionProvider.java
@@ -10,7 +10,7 @@ public class ConnectionProvider {
     public static Connection createConnection() throws ClassNotFoundException, SQLException {
 
         String user = "root";
-        String password = "root";
+        String password = "Bl00dB@nk";
         String url = "jdbc:mysql://localhost:3306/bloodbankdb";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
